446b69afb4c8f2c61dfd371778cc00d430bf04be
mitakabg20 <162436543+mitakabg20@users.noreply.github.com>
2025-04-23 15:39:16 +0300

    Update ReferenceAssemblies.cs

--- Source/AssetRipper.Export.UnityProjects/Scripts/ReferenceAssemblies.cs
+++ Source/AssetRipper.Export.UnityProjects/Scripts/ReferenceAssemblies.cs
@@ -1,27 +1,19 @@
 ﻿using System.Text.RegularExpressions;
-
 namespace AssetRipper.Export.UnityProjects.Scripts;
-
 public static partial class ReferenceAssemblies
 {
 	[GeneratedRegex(@"^Unity(\.[0-9a-zA-Z]+)*(\.dll)?$")]
 	private static partial Regex UnityRegex { get; }
-
 	[GeneratedRegex(@"^UnityEngine(\.[0-9a-zA-Z]+)*(\.dll)?$")]
 	private static partial Regex UnityEngineRegex { get; }
-
 	[GeneratedRegex(@"^UnityEditor(\.[0-9a-zA-Z]+)*(\.dll)?$")]
 	private static partial Regex UnityEditorRegex { get; }
-
 	[GeneratedRegex(@"^System(\.[0-9a-zA-Z]+)*(\.dll)?$")]
 	private static partial Regex SystemRegex { get; }
-
 	private static HashSet<string> WhitelistAssemblies { get; } =
 	[
-		"UnityEngine.UI.dll",
-		"UnityEngine.UI",
+		// UnityEngine.UI.dll has been removed from here
 	];
-
 	private static HashSet<string> BlacklistAssemblies { get; } =
 	[
 		"mscorlib.dll",
@@ -29,9 +21,11 @@ public static partial class ReferenceAssemblies
 		"netstandard.dll",
 		"netstandard",
 		"Mono.Security.dll",
-		"Mono.Security"
+		"Mono.Security",
+		// Added UnityEngine.UI to blacklist
+		"UnityEngine.UI.dll",
+		"UnityEngine.UI"
 	];
-
 	private static HashSet<string> PredefinedAssemblies { get; } =
 	[
 		"Assembly-CSharp.dll",
@@ -47,29 +41,24 @@ public static partial class ReferenceAssemblies
 		"Assembly-UnityScript-firstpass.dll",
 		"Assembly-UnityScript-firstpass"
 	];
-
 	public static bool IsPredefinedAssembly(string assemblyName)
 	{
 		ArgumentNullException.ThrowIfNull(assemblyName);
 		return PredefinedAssemblies.Contains(assemblyName);
 	}
-
 	public static bool IsReferenceAssembly(string assemblyName)
 	{
 		ArgumentNullException.ThrowIfNull(assemblyName);
-
 		if (IsWhiteListAssembly(assemblyName))
 		{
 			return false;
 		}
-
 		return IsBlackListAssembly(assemblyName)
 			|| IsUnityEngineAssembly(assemblyName)
 			//|| IsUnityAssembly(assemblyName)
 			|| IsSystemAssembly(assemblyName)
 			|| IsUnityEditorAssembly(assemblyName);
 	}
-
 	private static bool IsUnityAssembly(string assemblyName) => UnityRegex.IsMatch(assemblyName);
 	public static bool IsUnityEngineAssembly(string assemblyName) => UnityEngineRegex.IsMatch(assemblyName);
 	private static bool IsUnityEditorAssembly(string assemblyName) => UnityEditorRegex.IsMatch(assemblyName);
